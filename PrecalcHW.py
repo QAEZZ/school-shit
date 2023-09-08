b75607c8fac265f3f452e6344bddc5b5fc6cc5b5
QÆZZ <65181690+QAEZZ@users.noreply.github.com>
2023-09-08 14:07:49 -0500

    dis sum bulllshiitt

--- PrecalcHW.py
+++ PrecalcHW.py
@@ -1,5 +1,9 @@
 import json
+import os
+import pyautogui
+import time
 
+WRITE_INTERVAL=.02
 
 def questions() -> dict:
     doContinue: bool = True
@@ -7,7 +11,7 @@ def questions() -> dict:
     header = input("Header: ")
     while doContinue:
         question = input("Question: ")
-        questions.append(question)
+        questions.append(question.trim())
 
         moreQuestions: str = input("More questions? [Y/n] ").lower()
 
@@ -16,11 +20,11 @@ def questions() -> dict:
         else:
             doContinue = False
 
-
-    return {"header": header, "questions": [questions]}
+    return {"header": header, "questions": questions}
 
 
 def main() -> None:
+    os.system("cls")
     isContinuing = True
     data = []
 
@@ -33,12 +37,24 @@ def main() -> None:
             isContinuing = True
         else:
             isContinuing = False
+    
+    os.system("cls")
+    input("press enter to type to document, typing will start 5 seconds after.\n\nmake sure to have enable markdown detection tools>preferenceseseses")
+    time.sleep(5)
 
-        
-    json_data = json.dumps({"data": data})
+    json_data = json.loads(json.dumps({"data": data}))
 
-    print(f"\n\n{data}")
-    print(f"\n\n{json_data}")
+    sections = json_data["data"]
+    for section in sections:
+        h = f"**{section['header']}**"
+        print(h)
+        pyautogui.typewrite(f"{h}\n", WRITE_INTERVAL)
+        for q in section["questions"]:
+            formatted_q = f"**{q}\n\n".replace(". ", ".** ")
+            print(formatted_q)
+            pyautogui.typewrite(formatted_q, WRITE_INTERVAL)
+        print("\n\n")
+        pyautogui.typewrite("\n\n", WRITE_INTERVAL)
 
 
 if __name__ == "__main__":
@@ -50,20 +66,16 @@ if __name__ == "__main__":
     {
       "header": "Do the do for 1-2",
       "questions": [
-        [
-          "[1] Do that",
-          "[2] Do this"
-        ]
+        "[1] Do that",
+        "[2] Do this"
       ]
     },
     {
       "header": "Do the thingy for 3-5",
       "questions": [
-        [
-          "[3] Do thingy",
-          "[4] Do the do with the thingy",
-          "[5] Do the final thingy"
-        ]
+        "[3] Do thingy",
+        "[4] Do the do with the thingy",
+        "[5] Do the final thingy"
       ]
     }
   ]
